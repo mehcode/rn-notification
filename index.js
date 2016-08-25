241b58ca8f948bba47e8d26452ab9120f0c71606
Ryan Leckey <ryan@launchbadge.com>
2016-08-25 03:00:02 -0700

    Add JS

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
-import {NativeModules} from "react-native";
-const {RNNotification} = NativeModules;
+import {
+  NativeModules,
+  DeviceEventEmitter
+} from "react-native";
 
-export function create(options = {}) {
-  return RNNotification.create(options);
+const {
+  RNNotification
+} = NativeModules;
+
+const pressListeners = [];
+
+// True if we've emitted the initial notification press
+// that opened this activity
+let presentedInitialNotificationPress = false;
+
+function formatPressEvent(event) {
+  try {
+    event.payload = JSON.parse(event.payload || "");
+  } catch (err) {
+    event.payload = {};
+  }
+
+  return event;
 }
 
+DeviceEventEmitter.addListener("RNNotification:press", (nativeEvent) => {
+  // Format event
+  const event = formatPressEvent(nativeEvent);
+
+  // Emit
+  for (const listener of pressListeners) {
+    listener(event);
+  }
+});
+
 export default {
-  create,
+  // Create (local) notification
+  create(options = {}, payload = {}) {
+    return RNNotification.create(options, payload);
+  },
+
+  // Add listener (remote/local)
+  addListener(kind, listener) {
+    switch (kind) {
+      case "press":
+        // Register listener for normal press events
+        pressListeners.push(listener);
+
+        // Emit the initial notification press
+        if (!presentedInitialNotificationPress) {
+          RNNotification.getInitialNotificationPress((event) => {
+            listener(formatPressEvent(event));
+          });
+        }
+
+        break;
+    }
+  }
 };
